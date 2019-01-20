a1fa4296547182fe13e038c6dac7efd87a66a6bd
haney2121 <haney037332@gmail.com>
2019-01-20 00:19:25 -0600

    First commit

---
+++ graphql-playlist-server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
+//schema for roots
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-kxqfi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
+  .then((db) => {
+    
+    app.listen(process.env.PORT, () => {
+      console.log('Server is running');
+    });
+  }).catch(err => {
+    console.log(err);
+  });
+
+
+
+
