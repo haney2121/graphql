29b4396c41a0fea867dd941a525e54d1e6cdf2e8
haney2121 <haney037332@gmail.com>
2019-01-20 13:33:08 -0600

    upload both folders

--- graphql-playlist-server/index.js
+++ graphql-playlist-server/index.js
@@ -15,7 +15,7 @@ app.use('/graphql', graphqlHTTP({
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-kxqfi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
   .then((db) => {
     
-    app.listen(process.env.PORT, () => {
+    app.listen(4000, () => {
       console.log('Server is running');
     });
   }).catch(err => {
